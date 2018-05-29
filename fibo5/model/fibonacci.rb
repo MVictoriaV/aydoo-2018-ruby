cc40dec0ad4b05b3c6ec31cafa7ca5d09821c553
Victoria <MVictoriaV>
2018-05-29 01:23:56 -0300

    Implementacion del invertir serie

--- fibo5/model/fibonacci.rb
+++ fibo5/model/fibonacci.rb
@@ -1,21 +1,23 @@
 class Fibonacci
 
   attr_reader :sucesion
+  attr_reader :limite_serie
 
   def initialize(es_invertido=false, limite)
     if (limite.nil? or limite < 1)
       raise ArgumentError.new('El limite de la serie debe ser un numero > 0')
     end
-    calcular_serie(limite)
+    @limite_serie = limite
+    calcular_serie()
     invertir_serie(es_invertido)
   end
 
  private
- def calcular_serie(limite)
+ def calcular_serie()
   serie = Array.new()
   primer_valor = 0
   segundo_valor = 1
-  limite.times do
+  @limite_serie.times do
     serie.push(primer_valor)
     primer_valor,segundo_valor = segundo_valor,primer_valor+segundo_valor
   end
@@ -25,7 +27,11 @@ class Fibonacci
  private
  def invertir_serie(es_invertido)
   if es_invertido
-    @sucesion = [1, 1, 0]    
+    listado_invertido = Array.new
+    for i in (@limite_serie-1).downto(0)
+     listado_invertido.push(sucesion[i])
+    end
+    @sucesion = listado_invertido
   end
  end
 end
