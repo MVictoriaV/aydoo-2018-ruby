122e349dacfbbc08f989e7e3903b90bbae79d647
Victoria <MVictoriaV>
2018-05-29 02:25:27 -0300

    Implementacion del controller

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 require 'sinatra/json'
+require_relative './model/fibonacci'
 
 #/fibonacci/<N>[/lista|/sumatoria][?sentido=directo|inverso][&solo=pares|impares]
+
 get '/fibonacci/:limite' do 
 	limite_de_sucesion = params['limite']
-	sucesion = [0, 1, 1]
+	fibonacci = Fibonacci.new(limite_de_sucesion.to_i)
+	sucesion = fibonacci.sucesion
 	json({"fibonacci": { "limite": limite_de_sucesion.to_i, "lista": sucesion } })
 end
