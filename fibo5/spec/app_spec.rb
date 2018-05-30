c517e688bbcab753371ae6f94c6a9ad9e186a9dd
Victoria <MVictoriaV>
2018-05-29 22:05:27 -0300

    Refacor controller sumatoria + test

--- fibo5/spec/app_spec.rb
+++ fibo5/spec/app_spec.rb
@@ -9,33 +9,36 @@ describe 'Aplicacion Sinatra' do
     Sinatra::Application
   end
 
-  describe '/fibonacci/3' do
-    it 'deberia devolver la serie de Fibonacci con limite 3 en formato json' do
-      get '/fibonacci/3'
+  describe '/fibonacci/5' do
+    it 'deberia devolver la lista de Fibonacci con limite 5 en formato json' do
+      get '/fibonacci/5'
 
       expect(last_response).to be_ok
       cuerpo_parseado = JSON.parse(last_response.body)
-      expect(cuerpo_parseado['fibonacci']['lista']).to eq [0, 1, 1]
+      expect(cuerpo_parseado['fibonacci']['limite']).to eq 5
+      expect(cuerpo_parseado['fibonacci']['lista']).to eq [0, 1, 1, 2, 3]
     end
   end
 
-  describe '/fibonacci/3?sentido=inverso' do
-    it 'deberia devolver la serie invertida con limite 3 en formato json' do
-      get '/fibonacci/3?sentido=inverso'
+  describe '/fibonacci/5/sumatoria' do
+    it 'deberia devolver la sumatoria con valor 7 y limite 5 en formato json' do
+      get '/fibonacci/5/sumatoria'
 
       expect(last_response).to be_ok
       cuerpo_parseado = JSON.parse(last_response.body)
-      expect(cuerpo_parseado['fibonacci']['lista']).to eq [1, 1, 0]
+      expect(cuerpo_parseado['fibonacci']['limite']).to eq 5
+      expect(cuerpo_parseado['fibonacci']['sumatoria']).to eq 7
     end
   end
 
-  describe '/fibonacci/3?sentido=directo' do
-    it 'deberia devolver la serie normal con limite 3 en formato json' do
-      get '/fibonacci/3?sentido=directo'
+  describe '/fibonacci/5?sentido=inverso' do
+    it 'deberia devolver la lista invertida con 5 valores de la serie de Fibonacci y un limite 5 en formato json' do
+      get '/fibonacci/5?sentido=inverso'
 
       expect(last_response).to be_ok
       cuerpo_parseado = JSON.parse(last_response.body)
-      expect(cuerpo_parseado['fibonacci']['lista']).to eq [0, 1, 1]
+      expect(cuerpo_parseado['fibonacci']['limite']).to eq 5
+      expect(cuerpo_parseado['fibonacci']['lista']).to eq [3, 2, 1, 1, 0]
     end
   end
 end
