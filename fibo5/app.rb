f39b77ec32c9cbda2fed57661ec73577d567ff30
Victoria <MVictoriaV>
2018-05-29 22:04:49 -0300

    Refacor controller sumatoria

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -4,6 +4,16 @@ require_relative './model/fibonacci'
 
 #/fibonacci/<N>[/lista|/sumatoria][?sentido=directo|inverso][&solo=pares|impares]
 
+get '/fibonacci/:limite/sumatoria' do 
+
+	limite_de_sucesion = params['limite']
+	fibonacci = Fibonacci.new(limite_de_sucesion.to_i)
+	fibonacci_sumatoria = fibonacci.extend(Sumador)
+	fibonacci = fibonacci.extend(Sumador)
+	sumatoria_serie = fibonacci.sumatoria(fibonacci.sucesion)
+	json({"fibonacci": { "limite": limite_de_sucesion.to_i, "sumatoria": sumatoria_serie } })
+end
+
 get '/fibonacci/:limite' do 
 	limite_de_sucesion = params['limite']
 	es_inverso = (params['sentido'].nil? ? false : (params['sentido'].to_s == 'inverso'))
